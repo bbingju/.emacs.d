498ac6e775589e1ad31c2120f7b671b788138b62
ByungJu Hwang <pjhwang@gmail.com>
2016-05-14 08:05:31 +0900

    simplify the font setting

--- custom/bj-ui.el
+++ custom/bj-ui.el
@@ -45,23 +45,30 @@
 
 ;; face setting
 (when window-system
-  ;; (set-face-font 'default "-outline-Bitstream Vera Sans Mono-normal-normal-normal-mono-15-*-*-*-c-*-iso10646-1")
-  ;; (set-face-font 'default "-outline-DejaVu Sans Mono-normal-normal-normal-mono-15-*-*-*-c-*-iso10646-1")
-  (set-face-font 'default "-outline-Monaco-normal-normal-normal-*-*-*-*-*-p-*-iso10646-1")
-  ;; (set-face-font 'default "-outline-나눔고딕코딩-normal-normal-normal-mono-16-*-*-*-c-*-iso10646-1")
-  ;; (set-face-font 'default "-unknown-나눔고딕코딩-bold-normal-normal-*-*-*-*-*-d-0-iso10646-1")
-  ;; (set-face-font 'default "-outline-Consolas-normal-normal-normal-mono-*-*-*-*-c-*-fontset-consolas14")
-  ;; (set-face-font 'default "-outline-Consolas-normal-normal-normal-mono-15-*-*-*-c-*-iso10646-1")
-  ;; (set-face-font 'default "-microsoft-Consolas-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
-  ;; (set-fontset-font "fontset-default" 'hangul '("malgun gothic" . "unicode-bmp"))
-  ;; (set-fontset-font "fontset-default" 'hangul '("나눔고딕코딩" . "unicode-bmp"))
-  (when (equal current-language-environment "Korean")
-    (set-fontset-font "fontset-default" '(#x1100 . #xffdc)  '("NanumGothicCoding" . "unicode-bmp"))) ;; unicode region of Hangul
-  ;; (set-fontset-font "fontset-default" '(#x1100 . #xffdc)  '("malgun gothic" . "unicode-bmp")) ;; unicode region of Hangul
-  (set-fontset-font "fontset-default" 'kana '("ms mincho" . "unicode-bmp"))
-  (set-fontset-font "fontset-default" 'han '("ms mincho" . "unicode-bmp"))
-  ;; (set-fontset-font "fontset-default" 'cjk-misc '("ms mincho" . "unicode-bmp"))
-  ;; (set-fontset-font "fontset-default" '(#xe0bc . #xf66e)  '("new gulim" . "unicode-bmp")) ;; 유니코드 사용자 영역
+  (set-face-attribute 'default nil :family "Hack")
+  (set-face-attribute 'default nil :height 120)
+  ;; (set-default-font "Hack 11")
+  (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
   )
+
+;; (when window-system
+;;   ;; (set-face-font 'default "-outline-Bitstream Vera Sans Mono-normal-normal-normal-mono-15-*-*-*-c-*-iso10646-1")
+;;   ;; (set-face-font 'default "-outline-DejaVu Sans Mono-normal-normal-normal-mono-15-*-*-*-c-*-iso10646-1")
+;;   (set-face-font 'default "-outline-Monaco-normal-normal-normal-*-*-*-*-*-p-*-iso10646-1")
+;;   ;; (set-face-font 'default "-outline-나눔고딕코딩-normal-normal-normal-mono-16-*-*-*-c-*-iso10646-1")
+;;   ;; (set-face-font 'default "-unknown-나눔고딕코딩-bold-normal-normal-*-*-*-*-*-d-0-iso10646-1")
+;;   ;; (set-face-font 'default "-outline-Consolas-normal-normal-normal-mono-*-*-*-*-c-*-fontset-consolas14")
+;;   ;; (set-face-font 'default "-outline-Consolas-normal-normal-normal-mono-15-*-*-*-c-*-iso10646-1")
+;;   ;; (set-face-font 'default "-microsoft-Consolas-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
+;;   ;; (set-fontset-font "fontset-default" 'hangul '("malgun gothic" . "unicode-bmp"))
+;;   ;; (set-fontset-font "fontset-default" 'hangul '("나눔고딕코딩" . "unicode-bmp"))
+;;   (when (equal current-language-environment "Korean")
+;;     (set-fontset-font "fontset-default" '(#x1100 . #xffdc)  '("NanumGothicCoding" . "unicode-bmp"))) ;; unicode region of Hangul
+;;   ;; (set-fontset-font "fontset-default" '(#x1100 . #xffdc)  '("malgun gothic" . "unicode-bmp")) ;; unicode region of Hangul
+;;   (set-fontset-font "fontset-default" 'kana '("ms mincho" . "unicode-bmp"))
+;;   (set-fontset-font "fontset-default" 'han '("ms mincho" . "unicode-bmp"))
+;;   ;; (set-fontset-font "fontset-default" 'cjk-misc '("ms mincho" . "unicode-bmp"))
+;;   ;; (set-fontset-font "fontset-default" '(#xe0bc . #xf66e)  '("new gulim" . "unicode-bmp")) ;; 유니코드 사용자 영역
+;;   )
 
 (provide 'bj-ui)
