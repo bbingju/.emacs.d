8ed39b9b796567cd2c0f27942885b27d2abeaf87
Phil Hwang <pjhwang@gmail.com>
2017-01-24 17:58:04 +0900

    Remove an old config about `helm-projectile'

--- custom/bj-programming.el
+++ custom/bj-programming.el
@@ -19,11 +19,6 @@
              magit-status)
   :bind ("C-x g" . magit-status))
 
-;;; helm-projectile
-(projectile-global-mode)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
-
 ;;
 ;;; For Python programming
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
