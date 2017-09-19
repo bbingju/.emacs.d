ada41c576bc5285e4fe8f9107b44942e03723de3
Phil Hwang <pjhwang@gmail.com>
2017-09-19 10:51:01 +0900

    Use original melpa archive instead mirror

--- init.el
+++ init.el
@@ -40,7 +40,7 @@
 (when-version-24
  (require 'package)
  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
- (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+ (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
  (package-initialize))
 
