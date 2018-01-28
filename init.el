21eb497f504f264cdfde3de12560275d2f04d560
Byung-Ju Hwang <pjhwang@gmail.com>
2018-01-28 20:00:11 +0900

    Fix URL for org-mode ELPA repository

--- init.el
+++ init.el
@@ -45,7 +45,7 @@
  (require 'package)
  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
- (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+ (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
  (package-initialize))
 
 ;; disable the annoying bell ring
