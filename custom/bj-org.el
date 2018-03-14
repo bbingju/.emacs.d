8de01ce1c2698c5232a16dade87f636ea3d7f516
Byung Ju Hwang <pjhwang@gmail.com>
2018-03-14 16:43:46 +0900

    Use 'use-package' to initialize Org-mode

--- custom/bj-org.el
+++ custom/bj-org.el
@@ -1,30 +1,20 @@
 ;;; bj-orgmode.el
 
+(use-package org
+  :ensure t
+  :init (setq org-directory "~/Dropbox/org"
+	      org-agenda-files '("~/Dropbox/org"))
+  :mode (("\\.org\\'" . org-mode))
+  :bind (("C-c l" . org-store-link)
+	 ("C-c c" . org-capture)
+	 ("C-c a" . org-agenda)))
+
 (setq org-export-html-style-include-scripts nil
       org-export-html-style-include-default nil)
 (setq org-export-html-style
       "<link rel=\"stylesheet\" type=\"text/css\" href=\"solarized-light.css\" />")
 
-;; Set to the location of your Org files on your local system
-(setq org-directory "~/Dropbox/org")
-(setq org-agenda-files '("~/Dropbox/org"))
-
-;; Set to the name of the file where new notes will be stored
-(setq org-mobile-inbox-for-pull (expand-file-name "inbox.org" org-directory))
-(setq org-mobile-files '("~/Dropbox/org/todo.org"))
-
-;; Set to <your Dropbox root directory>/MobileOrg.
-(setq org-mobile-agenda-files '("~/Dropbox/org/todo.org"
-				"~/Dropbox/org/study.org"
-				"~/Dropbox/org/travels.org"))
-(setq org-mobile-directory "~/Dropbox/MobileOrg-new")
-
 (setq org-default-notes-file (concat org-directory "/notes.org"))
-
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c b") 'org-iswitch)
 
 (set-register ?l (cons 'file "~/Dropbox/org/links.org"))
 
@@ -41,19 +31,14 @@
                                                          (emacs-lisp . t)))
 
 ;; org-present: simple presentation plug-in
-;; (add-to-list 'load-path "~/path/to/org-present")
-(autoload 'org-present "org-present" nil t)
-
-(add-hook 'org-present-mode-hook
-          (lambda ()
-            (org-present-big)
-            (org-display-inline-images)))
-
-(add-hook 'org-present-mode-quit-hook
-          (lambda ()
-            (org-present-small)
-            (org-remove-inline-images)))
-
+(use-package org-present
+  :ensure t
+  :hook ((org-present-mode . (lambda ()
+				    (org-present-big)
+				    (org-display-inline-images)))
+	 (org-present-mode-quit . (lambda ()
+				    (org-present-small)
+				    (org-remove-inline-images)))))
 
 ;; Calendar setting
 (require 'calendar)		      ; it's built-in.
