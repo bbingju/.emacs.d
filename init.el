883753fb5ed3b23dc3b288893780241b993a90f6
Byung-Ju Hwang <pjhwang@gmail.com>
2018-03-15 13:18:25 +0900

    Change a initial logic for elpa package

--- init.el
+++ init.el
@@ -41,12 +41,19 @@
 		(cons 'progn body)))
 
 ;; Package (ELPA)
-(when-version-24
- (require 'package)
- (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
- (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
- (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
- (package-initialize))
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "org" (concat proto "://orgmode.org/elpa/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
 
 ;; disable the annoying bell ring
 (setq ring-bell-function 'ignore)
