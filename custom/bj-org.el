d62f37c5374bb182d74c2726416071e1935b2039
Phil Hwang <pjhwang@gmail.com>
2018-09-11 10:55:32 +0900

    org: Refactor configurations

--- custom/bj-org.el
+++ custom/bj-org.el
@@ -2,38 +2,48 @@
 
 (use-package org
   :ensure t
-  :init (setq org-directory "~/Dropbox/org"
-	      org-agenda-files '("~/Dropbox/org"))
+
+  :init (setq org-directory "~/org"
+	      org-agenda-files '("~/org")
+              org-default-notes-file (concat org-directory "/notes.org")
+              org-export-coding-system 'utf-8)
+
   :mode (("\\.org\\'" . org-mode))
+
   :bind (("C-c l" . org-store-link)
 	 ("C-c c" . org-capture)
-	 ("C-c a" . org-agenda)))
+	 ("C-c a" . org-agenda))
+
+  :config
+  (set-register ?l (cons 'file "~/org/links.org"))
+  ;; refer to http://orgmode.org/manual/Template-elements.html#Template-elements
+  (setq org-capture-templates
+        '(("l"                          ; hotkey
+           "Link"                       ; name
+           entry                        ; type
+           (file+headline org-default-notes-file "Links") 
+           "* %? %^L %^g \n%T" :prepend t)
+          ("t" "To Do Item" entry (file+headline org-default-notes-file "To Do Items") 
+           "* %?\n%T" :prepend t)
+          ("r" "To Read Item" entry (file+headline org-default-notes-file "To Read Items") 
+           "* %?\n%T" :prepend t)))
+
+  (org-babel-do-load-languages 'org-babel-load-languages '((ditaa . t)
+                                                           (emacs-lisp . t)))
+
+  (setq org-feed-alist
+        '(("Slashdot"
+           "http://rss.slashdot.org/Slashdot/slashdot"
+           "~/org/feeds.org" "Slashdot Entries")))
+
+  ;; Calendar setting
+  (require 'calendar)		      ; it's built-in.
+  (calendar-set-date-style 'iso)      ; set the "year/month/day" style
+  )
 
 (use-package org-bullets
   :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-(setq org-export-html-style-include-scripts nil
-      org-export-html-style-include-default nil)
-(setq org-export-html-style
-      "<link rel=\"stylesheet\" type=\"text/css\" href=\"solarized-light.css\" />")
-
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-
-(set-register ?l (cons 'file "~/Dropbox/org/links.org"))
-
-;; refer to http://orgmode.org/manual/Template-elements.html#Template-elements
-(setq org-capture-templates
-      '(("l" "Link" entry (file+headline "~/Dropbox/org/links.org" "Links") 
-         "* %? %^L %^g \n%T" :prepend t)
-        ("t" "To Do Item" entry (file+headline "~/Dropbox/org/i.org" "To Do Items") 
-         "* %?\n%T" :prepend t)
-        ("r" "To Read Item" entry (file+headline "~/Dropbox/org/toread.org" "To Read Items") 
-         "* %?\n%T" :prepend t)))
-
-(org-babel-do-load-languages 'org-babel-load-languages '((ditaa . t)
-                                                         (emacs-lisp . t)))
+  :hook (org-mode . (lambda () (org-bullets-mode 1))))
 
 ;; org-present: simple presentation plug-in
 (use-package org-present
@@ -44,9 +54,5 @@
 	 (org-present-mode-quit . (lambda ()
 				    (org-present-small)
 				    (org-remove-inline-images)))))
-
-;; Calendar setting
-(require 'calendar)		      ; it's built-in.
-(calendar-set-date-style 'iso)	      ; set the "year/month/day" style
 
 (provide 'bj-org)
