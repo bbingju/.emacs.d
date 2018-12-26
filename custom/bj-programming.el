a096617f26da366e2bdac2b7c19b1328c5747d96
Byung Ju Hwang <pjhwang@gmail.com>
2018-12-26 20:23:08 +0900

    Add new package 'yasnippet-snippets'

--- custom/bj-programming.el
+++ custom/bj-programming.el
@@ -9,6 +9,10 @@
   :diminish yas-minor-mode
   :config (yas-global-mode 1))
 
+(use-package yasnippet-snippets
+  :ensure t
+  :after yasnippet)
+
 ;; editorconfig
 (use-package editorconfig
   :ensure t
