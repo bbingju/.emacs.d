3a8b8a68f13e9bcc156457d317d18d381b2a370f
Phil Hwang <pjhwang@gmail.com>
2019-01-10 15:33:55 +0900

    Remove disabled code

--- custom/bj-ui.el
+++ custom/bj-ui.el
@@ -64,57 +64,6 @@
 
   (when-windows (fontutil/set-font "d2coding-14")
         	(setq-default line-spacing 4))
-
-  ;; ;; set ligatures
-  ;; (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
-  ;;       	 (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
-  ;;       	 (36 . ".\\(?:>\\)")
-  ;;       	 (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
-  ;;       	 (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
-  ;;       	 (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
-  ;;       	 (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
-  ;;       	 (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
-  ;;       	 (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
-  ;;       	 (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
-  ;;       	 (48 . ".\\(?:x[a-zA-Z]\\)")
-  ;;       	 (58 . ".\\(?:::\\|[:=]\\)")
-  ;;       	 (59 . ".\\(?:;;\\|;\\)")
-  ;;       	 (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
-  ;;       	 (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
-  ;;       	 (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
-  ;;       	 (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
-  ;;       	 (91 . ".\\(?:]\\)")
-  ;;       	 (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
-  ;;       	 (94 . ".\\(?:=\\)")
-  ;;       	 (119 . ".\\(?:ww\\)")
-  ;;       	 (123 . ".\\(?:-\\)")
-  ;;       	 (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
-  ;;       	 (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
-  ;;       	 )))
-  ;;   (dolist (char-regexp alist)
-  ;;     (set-char-table-range composition-function-table (car char-regexp)
-  ;;       		    `([,(cdr char-regexp) 0 font-shape-gstring]))))
   )
-
-
-;; (when window-system
-;;   ;; (set-face-font 'default "-outline-Bitstream Vera Sans Mono-normal-normal-normal-mono-15-*-*-*-c-*-iso10646-1")
-;;   ;; (set-face-font 'default "-outline-DejaVu Sans Mono-normal-normal-normal-mono-15-*-*-*-c-*-iso10646-1")
-;;   (set-face-font 'default "-outline-Monaco-normal-normal-normal-*-*-*-*-*-p-*-iso10646-1")
-;;   ;; (set-face-font 'default "-outline-나눔고딕코딩-normal-normal-normal-mono-16-*-*-*-c-*-iso10646-1")
-;;   ;; (set-face-font 'default "-unknown-나눔고딕코딩-bold-normal-normal-*-*-*-*-*-d-0-iso10646-1")
-;;   ;; (set-face-font 'default "-outline-Consolas-normal-normal-normal-mono-*-*-*-*-c-*-fontset-consolas14")
-;;   ;; (set-face-font 'default "-outline-Consolas-normal-normal-normal-mono-15-*-*-*-c-*-iso10646-1")
-;;   ;; (set-face-font 'default "-microsoft-Consolas-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
-;;   ;; (set-fontset-font "fontset-default" 'hangul '("malgun gothic" . "unicode-bmp"))
-;;   ;; (set-fontset-font "fontset-default" 'hangul '("나눔고딕코딩" . "unicode-bmp"))
-;;   (when (equal current-language-environment "Korean")
-;;     (set-fontset-font "fontset-default" '(#x1100 . #xffdc)  '("NanumGothicCoding" . "unicode-bmp"))) ;; unicode region of Hangul
-;;   ;; (set-fontset-font "fontset-default" '(#x1100 . #xffdc)  '("malgun gothic" . "unicode-bmp")) ;; unicode region of Hangul
-;;   (set-fontset-font "fontset-default" 'kana '("ms mincho" . "unicode-bmp"))
-;;   (set-fontset-font "fontset-default" 'han '("ms mincho" . "unicode-bmp"))
-;;   ;; (set-fontset-font "fontset-default" 'cjk-misc '("ms mincho" . "unicode-bmp"))
-;;   ;; (set-fontset-font "fontset-default" '(#xe0bc . #xf66e)  '("new gulim" . "unicode-bmp")) ;; 유니코드 사용자 영역
-;;   )
 
 (provide 'bj-ui)
