da044b3e0d9d71d2517b36883f836582b2640762
Byung-Ju Hwang <pjhwang@gmail.com>
2019-06-25 13:11:44 +0900

    Update configurations of python mode

--- custom/lang-python.el
+++ custom/lang-python.el
@@ -3,20 +3,33 @@
 
 (use-package python
   :mode ("\\.py\\'" . python-mode)
-  :interpreter ("python3" . python-mode))
+  :interpreter ("python3" . python-mode)
+  :config
+  (use-package elpy
+    :ensure t
+    :init
+    (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+    :config
+    (setq elpy-rpc-python-command "python3")
+    (setq python-shell-interpreter "python3")
+    (setq python-shell-interpreter-args "-i")
+    :bind (:map elpy-mode-map
+              ("M-." . elpy-goto-definition)
+              ("M-," . pop-tag-mark)))
+  (elpy-enable))
+
+(use-package pip-requirements
+  :ensure t
+  :config
+  (add-hook 'pip-requirements-mode-hook #'pip-requirements-auto-complete-setup))
+
+(use-package py-autopep8
+  :ensure t)
 
 (use-package company-jedi
   :ensure t
   :after company
   :config (add-to-list 'company-backends 'company-jedi))
-
-
-(use-package elpy
-  :ensure t
-  :config (elpy-enable)
-  (setq elpy-rpc-python-command "python3")
-  (setq python-shell-interpreter "python3")
-  (setq python-shell-interpreter-args "-i"))
 
 (use-package flycheck-pyflakes
   :ensure t
